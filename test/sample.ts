71bd5a5e86c6293539887c4e0c3866606fd357bf
Fred Souza <fred@Macbook.local>
2015-08-31 22:58:55 -0300

    Cleaning tests

--- test/sample.ts
+++ test/sample.ts
@@ -1,9 +1,11 @@
-/// <reference path="../defs/tsd.d.ts" />
+declare var require, describe, it;
 var Assert = require("assert");
 
 describe("Sample test case", () => {
+	
 	it("should pass", (done) => {
 		Assert.equal(true, true);
 		done();
 	});
+	
 });
